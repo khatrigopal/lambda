954faa1aabd4c06d9a027e385df1270cd53eb537
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-07 18:48:48 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,20 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+### Backend ###
+# S3
+###############
+
+terraform {
+  backend "s3" {
+    bucket = "khatrig-githubaction"
+    key = lambda.tfstate"
+    region = "us-east-1"
+  }
+}
+######### S3##############
+
 resource "aws_iam_role" "lambda_role" {
 name   = "lambda_role"
 assume_role_policy = <<EOF
