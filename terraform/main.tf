50686243758424acca02e8c41de8f3e969b081f3
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-07 19:05:05 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -71,12 +71,12 @@ resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
 
 data "archive_file" "zip_the_python_code" {
 type        = "zip"
-source_dir  = "${path.module}/python/"
-output_path = "${path.module}/python/hello-python.zip"
+source_file = "index.py"
+output_path = "welcome.zip"
 }
  
 resource "aws_lambda_function" "terraform_lambda_func" {
-filename                       = "${path.module}/python/hello-python.zip"
+filename                       = data.archive_file.zip_the_python_code.output_path
 function_name                  = "Spacelift_Test_Lambda_Function"
 role                           = aws_iam_role.lambda_role.arn
 handler                        = "index.lambda_handler"
